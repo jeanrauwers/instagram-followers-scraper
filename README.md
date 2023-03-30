9fe10ee224d674616d6ca87df4727b86d4d4e3e4
Jean Rauwers <jeanrauwers@skyscanner.net>
2023-03-30 12:25:43 +0100

    Add scraper to github

---
+++ README.md
@@ -0,0 +1,10 @@
+# instagram-get-follower-scraper
+
+This python script can automatically scrape a user total followers and save it on a text file
+
+# How to use:
+
+1. Install requirements using terminal or command line. You should run this command `pip install -r requirements.txt`.
+2. Rename .env.example to .env and add ig username in example file.
+3. Open a terminal or cmd again and run the bot using this command: `python run.py`.
+4. And that's it. You get your total followers number in a text file in the root folder.
---
+++ run.py
@@ -0,0 +1,45 @@
+import time, os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager as CM
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+TIMEOUT = 15
+USERNAME = os.getenv('USERNAME')
+
+
+def scrape():
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")   # Uncomment this option to run it headless
+    options.add_argument('--no-sandbox')
+    options.add_argument("--log-level=3")
+    mobile_emulation = {
+        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
+    options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    bot = webdriver.Chrome(executable_path=CM().install(), options=options)
+    instagram = 'https://www.instagram.com/'
+    url = instagram + str(USERNAME)
+
+    bot.get(url)
+
+    time.sleep(3.5)
+
+    print('[Info] - Scraping Followers...')
+    followers_span = bot.find_element(By.CSS_SELECTOR,
+    "span[title]")
+
+    total_followers = followers_span.get_attribute('title')
+
+    print('[Info] - Saving...')
+    print('[DONE] - Your total followers are saved in followers.txt file!')
+
+    with open('followers.txt', 'r+') as file:
+        file.seek(0, 0)  
+        file.write(total_followers) 
+
+
+if __name__ == '__main__':
+    scrape()
