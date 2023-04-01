44a85fae16c5b13b6fea5370cfa71eb1f554e290
Jean Rauwers <jeanrauwers@skyscanner.net>
2023-04-01 12:54:34 +0100

    Fix userAgent

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-import time, os
+import time
+import os
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager as CM
 from dotenv import load_dotenv, find_dotenv
 
@@ -10,36 +13,41 @@ TIMEOUT = 15
 USERNAME = os.getenv('USERNAME')
 dirname = os.path.dirname(__file__)
 
+
 def scrape():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")   # Uncomment this option to run it headless
+    # Uncomment this option to run it headless
+    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
-    mobile_emulation = {
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
-    options.add_experimental_option("mobileEmulation", mobile_emulation)
+    # Add a Mozila firefox user-agent so instagram doesn't block you.
+    options.add_argument(
+        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36')
 
     bot = webdriver.Chrome(executable_path=CM().install(), options=options)
     instagram = 'https://www.instagram.com/'
     url = instagram + str(USERNAME)
 
-    bot.get(url)
-
-    time.sleep(3.5)
-
     print('[Info] - Scraping Followers...')
-    followers_span = bot.find_element(By.CSS_SELECTOR,
-    "span[title]")
+    try:
+        bot.get(url)
+        time.sleep(3.5)
+        followers_span = bot.find_element(By.CSS_SELECTOR,
+                                          "span[title]")
 
-    total_followers = followers_span.get_attribute('title')
+        total_followers = followers_span.get_attribute('title')
 
-    print('[Info] - Saving...')
-    print('[Info] - Total of ' + total_followers + ' followers')
-    print('[DONE] - Your total followers are saved in followers.txt file!')
+        print(total_followers)
+        print('[Info] - Saving...')
+        print('[Info] - Total of ' + total_followers + ' followers')
 
-    with open(os.path.join(dirname, 'followers.txt'), 'r+') as file:
-        file.seek(0, 0)  
-        file.write(total_followers) 
+        with open(os.path.join(dirname, 'followers.txt'), 'r+') as file:
+            file.seek(0, 0)
+            file.write(total_followers)
+        print('[DONE] - Your total followers are saved in followers.txt file!')
+
+    except NoSuchElementException:
+        print(NoSuchElementException)
 
 
 if __name__ == '__main__':
